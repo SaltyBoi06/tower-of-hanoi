0a7250a41a2806548fa80d2b6b77eb66b05ca5db
Sam Hartelius <sam.hartelius@gmail.com>
2025-09-29 16:03:36 +0200

    Add files via upload

--- kod.js
+++ kod.js
@@ -4,3 +4,17 @@ let pinneA = [ 1, 2 ];
 let pinneB = [  ];
 let pinneC = [  ];
 
+let isDone = false;
+
+do { 
+    pinneA.pop(1)
+    pinneB.push(2)
+    isDone = true
+}
+while (
+    isDone == false
+);
+
+console.log(pinneA)
+console.log(pinneB)
+console.log(pinneC)
